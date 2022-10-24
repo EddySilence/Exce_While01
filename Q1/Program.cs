6b20ae8c73ee3d12dd3076a95e36bde702e18d34
eddy <eddy19991118@gmail.com>
2022-10-24 19:31:02 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //題目：1+2+3+...+N<105。請問N是多少?
+
+            int sum = 0;//總數
+            int number = 1;//1~n的數字
+
+            while (sum < 105)
+            {
+                sum += number;
+                number++;
+            }
+            sum -= number;
+
+            Console.WriteLine($"N是{sum}");
+
+        }
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //題目：使用while，在畫面顯示所有可以整除300的整數
+
+            int number = 1;
+            while (number <= 300)
+            {
+                if (300 % number == 0)
+                {
+                    Console.WriteLine(number);
+                }
+                number++;
+            }
+
+
+        }
+    }
+}
